9e49b72da9f06522a41cf0615a86fec725d5241e
Ade Balogun <abalogun316@gmail.com>
2021-05-28 18:11:23 +0100

    more advanced configs and introduction of celery

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,23 @@
+import os
 from flask import Flask
+from celery import Celery
+
+def make_celery():
+    my_celery = Celery(__name__, broker=os.environ["CELERY_BROKER_URL"])
+    return my_celery
 
 def create_app():
     app = Flask(__name__)
 
-    from .routes import main, auth
+    celery.conf.update(app.config)
+
+    from .routes import main, auth, list, rent, process
+
     app.register_blueprint(main)
     app.register_blueprint(auth)
+    app.register_blueprint(list)
+    app.register_blueprint(rent)
+    app.register_blueprint(process)
     return app
+
+celery = make_celery()
