9af68f028ab4ce1520d23a65afaf20fe6eee4b99
Ade Balogun <abalogun316@gmail.com>
2021-06-05 11:53:55 +0100

    basic routing:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Flash from './components/base/Flash';
 import Navbar from './components/base/Navbar';
 import Footer from './components/base/Footer';
 import Main from './components/pages/Main';
+import Shop from './components/pages/Shop';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import Logout from './components/pages/Logout';
+
 //import MetaData from './components/base/MetaData';
 
 const App = () => {
-  //const [myUsers, setMyUsers] = React.useState([]);
-  const [myTestimonials, setMyTestimonials] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [flashMessages, setFlashMessages] = useState([]);
+  const [cartSize, setCartSize] = useState(null);
   useEffect(() => {
-    fetch('/test').then(res => res.json()).then(data => {
-      setMyTestimonials(data.testimonials);
+    fetch('/login_status')
+    .then(res => res.json())
+    .then(data => {
+      setIsLoggedIn(data.is_logged_in);
+      setCartSize(data.cart_size);
     });
   }, []);
   return (
-    <div className="App">
-      <Navbar loggedIn={false} />
-      <Main testimonials={myTestimonials}/>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar isLoggedIn={isLoggedIn} cartSize={cartSize} />
+        <Flash flashMessages={flashMessages} />
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route exact path="/inventory">
+            <Shop />
+          </Route>
+          <Route exact path="/login">
+            <Login
+              isLoggedIn={isLoggedIn}
+              setIsLoggedIn={setIsLoggedIn}
+              setFlashMessages={setFlashMessages}
+              setCartSize={setCartSize} />
+          </Route>
+          <Route exact path="/register">
+            <Register isLoggedIn={isLoggedIn} />
+          </Route>
+          <Route exact path="/logout">
+            <Logout />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
