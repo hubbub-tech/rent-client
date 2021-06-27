6d500f9d09ecca2ab5548c92caa88b2a240c827d
Ade Balogun <abalogun316@gmail.com>
2021-06-27 16:08:14 +0100

    formatting from database data

--- src/helper.js
+++ src/helper.js
@@ -1,17 +1,20 @@
-const formatName = (unformattedName) => {
-  let _formattedName = unformattedName.split(",")
-  let formattedName = _formattedName.join(" ")
-  return formattedName;
-};
+import moment from 'moment';
 
-// Eventually build in messages
-const isActiveRental = (startDate, endDate) => {
-  const todayDate = new Date();
-  if (startDate < todayDate && endDate > todayDate) {
-    return True;
-  } else if (startDate > todayDate) {
-    return False;
-  } else {
-    return False;
-  }
+const printMoney = (floatAmount) => {
+  const errorMsg = "printMoney can only be used on type float."
+  console.assert(typeof floatAmount === "float", {floatAmount, errorMsg});
+  const money = Number.parseFloat(floatAmount).toFixed(2);
+  return `${money}`;
 }
+
+const stringToMoment = (stringDate, formatStr = "YYYY-MM-DD") => {
+  const momentDate = moment.utc(stringDate, formatStr);
+  return momentDate;
+}
+
+const printMoment = (momentDate, formatStr = "MMMM Do YYYY") => {
+  const printDate = momentDate.format(formatStr);
+  return printDate;
+}
+
+export { stringToMoment, printMoment, printMoney };
