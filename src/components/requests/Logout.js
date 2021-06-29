26881a917274d127cfa5ee010398af4d48921ab3
Ade Balogun <abalogun316@gmail.com>
2021-06-29 22:31:24 +0100

    tidy code

--- src/components/requests/Logout.js
+++ src/components/requests/Logout.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
 const Logout = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  fetch('/logout').then(res => res.json()).then(data => {
-    setIsLoggedIn(data.is_logged_in);
-    console.log("isLoggedIn", isLoggedIn)
-  });
+  fetch('/logout')
   return <Redirect to='/' />
 }
 
