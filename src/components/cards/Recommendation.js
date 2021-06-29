3a28e8fd18c6dd187f7e485d51273d6ed739ad66
Ade Balogun <abalogun316@gmail.com>
2021-06-29 22:34:14 +0100

    card for 'recommended items, still developing

---
+++ src/components/cards/Recommendation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Recommendation = ({ urlBase, item }) => {
+  return (
+    <a className="custom-card" href={`/inventory/i/id=${item.id}`}>
+      <div className="card mt-1 mb-3" id={`item-${item.id}`}>
+        <div className="card-body">
+          <img
+            className="card-img img-fluid"
+            src={`${urlBase}/${item.id}.jpg`}
+            alt={item.name}
+          />
+          <h6 className="card-title">{item.name}</h6>
+          <p className="card-text mt-3 mb-1">From {item.price}/day</p>
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default Recommendation;
