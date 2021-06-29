a588555e6c23d2bc6677cc5846c895238eac7923
Ade Balogun <abalogun316@gmail.com>
2021-06-29 22:29:39 +0100

    make rentals pretty

--- src/components/pages/Rentals.js
+++ src/components/pages/Rentals.js
@@ -24,19 +24,21 @@ const Rentals = () => {
   return (
     <main>
       <div className="container-md">
-        <div className="container-md">
-          <div className="row">
-            <div className="col-md-1"></div>
-            <div className="col-md-11 mt-5">
-              <h1>My Rentals</h1>
-              <p>See your rental history here, and manage current and past orders.</p>
-            </div>
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-11 mt-5">
+            <h1 className="text-start">My Rentals</h1>
+            <p className="text-start">See your rental history here, and manage current and past orders.</p>
           </div>
         </div>
-        <hr />
         {orders.length !== 0 &&
           <div className="row">
-            {orders.map((order) => <OrderCard urlBase={urlBase} order={order} key={order.id} />)}
+            <div className="col-md-1"></div>
+            <div className="col-md-10">
+              <hr />
+              {orders.map((order) => <OrderCard urlBase={urlBase} order={order} key={order.id} />)}
+            </div>
+            <div className="col-md-1"></div>
           </div>
         }
         {orders.length === 0 && <p className="text-center">You haven't placed any orders yet :(</p>}
