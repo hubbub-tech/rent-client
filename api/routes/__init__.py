93b34f582bc4e8db9a5fba2a8d4ddebabd9671b5
Ade Balogun <abalogun316@gmail.com>
2021-07-11 03:34:27 +0100

    moved server to its own repo

--- api/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .main import bp as main
-from .auth import bp as auth
-from .list import bp as list
-from .rent import bp as rent
-from .process import bp as process
--- api/tools/build/__init__.py
+++
@@ -1,7 +0,0 @@
-from .create import create_user, create_item, create_review
-from .create import create_reservation, create_logistics
-from .create import create_extension, create_order
-
-from .forms import validate_edit_account, validate_edit_password, upload_image
-from .forms import validate_rental_bounds, validate_listing
-from .forms import validate_login, validate_registration
