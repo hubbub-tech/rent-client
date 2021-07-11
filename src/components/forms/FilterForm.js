c1db5b4c154d367bc3aa029a4cd9c66bec316281
Ade Balogun <abalogun316@gmail.com>
2021-07-11 03:32:10 +0100

    started a method for filtering on inventory page

---
+++ src/components/forms/FilterForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FilterForm = ({ items, setFilters }) => {
+  return (
+    <div className="row">
+      <div className="col-12 my-2">
+        <h5 className="text-start">Filters</h5>
+        
+      </div>
+    </div>
+  );
+}
+
+export default FilterForm;
