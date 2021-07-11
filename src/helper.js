8352a52b84d3cb7ee61758389b0de2805982e1de
Ade Balogun <abalogun316@gmail.com>
2021-07-11 03:33:36 +0100

    transated pricing function into JS from python

--- src/helper.js
+++ src/helper.js
@@ -19,4 +19,25 @@ const printDate = (stringDate, formatStr = "MMMM Do YYYY") => {
   return printDate;
 }
 
-export { stringToMoment, printDate, printMoney };
+const expDecay = (retailPrice, timeNow, discount = .50, timeTotal = 28) => {
+  // Where discount is issued at timeTotal
+  let basePrice = retailPrice / 10;
+  if (timeNow > 180) {
+    timeTotal = 56;
+  }
+  let compound = retailPrice / 90;
+  let a = compound * Math.pow(10, - Math.log((1 - discount), 10) / (timeTotal - 1));
+  let r = 1 - (compound / a);
+  let y = a * Math.pow(1 - r, timeNow);
+
+  //calculate the cost of the rental to the user
+  let integTime = y / Math.log(1 - r);
+  let integ0 = a * (1 - r) / Math.log(1 - r);
+  let costToDate = basePrice + integTime - integ0;
+  if (costToDate < basePrice) {
+    return basePrice;
+  }
+  return costToDate;
+}
+
+export { stringToMoment, printDate, printMoney, expDecay };
