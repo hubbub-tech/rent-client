e15502c6ee4ba9884d7603d6dc0ac3baeeb13336
Ade Balogun <abalogun316@gmail.com>
2021-10-28 00:48:32 -0400

    code reorg, with last commit

--- src/components/base/Navbar.js
+++ src/components/base/Navbar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
+import CartIcon from '../icons/CartIcon';
 import SearchForm from '../forms/SearchForm';
 
 const Navbar = ({ isLoggedIn }) => {
+  const history = useHistory();
   const hubbubId = Cookies.get('hubbubId');
   const cartSize = Cookies.get('cartSize');
+
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  const onClickCheckout = () => history.push('/checkout');
   return (
     <nav className="navbar navbar-expand-lg navbar-light hubbub-background">
       <div className="container-fluid">
@@ -66,19 +71,7 @@ const Navbar = ({ isLoggedIn }) => {
             }
             <SearchForm />
             {isNavCollapsed &&
-              <a href="/checkout">
-                <svg
-                  width="2rem"
-                  height="2rem"
-                  viewBox="0 0 16 16"
-                  className="bi bi-cart3 navbar-brand"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fillRule="evenodd"
-                    d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                </svg>
-              </a>
+              <CartIcon fill={"black"} size={"2rem"} onClick={onClickCheckout} />
             }
           </ul>
         </div>
