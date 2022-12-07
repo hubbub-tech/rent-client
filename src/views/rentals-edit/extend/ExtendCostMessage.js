a9694209a4fc2451dc48c2ae188331051f18b0df
Ade Balogun <abalogun316@gmail.com>
2022-12-07 18:34:03 -0500

    more visibility into extension cost breakdown

--- src/views/rentals-edit/extend/ExtendCostMessage.js
+++ src/views/rentals-edit/extend/ExtendCostMessage.js
@@ -5,6 +5,8 @@ import { printDate, printMoney, expDecay } from '../../utils.js';
 export const ExtendCostMessage = ({ order, dtEnded }) => {
 
   const [extendCost, setExtendCost] = useState(undefined);
+  const [extendDeposit, setExtendDeposit] = useState(undefined);
+  const [extendTax, setExtendTax] = useState(undefined);
 
   useEffect(() => {
     let dtStarted = new Date(order.ext_dt_end * 1000);
@@ -16,6 +18,8 @@ export const ExtendCostMessage = ({ order, dtEnded }) => {
       let getQuote = expDecay(order.item.retail_price, daysDuration);
 
       setExtendCost(getQuote);
+      setExtendDeposit(getQuote * .25);
+      setExtendTax(getQuote * .08875);
     } else {
       setExtendCost(undefined);
     }
@@ -24,6 +28,10 @@ export const ExtendCostMessage = ({ order, dtEnded }) => {
   return (extendCost)
     ? <span className="fw-bold text-dark">
         Extend for: <span className="text-success">{ printMoney(extendCost) }</span>
+        <p className="fw-normal">
+        <small className="text-muted"> + { printMoney(extendDeposit) } (deposit)</small>
+        <small className="text-muted"> + { printMoney(extendTax) } (tax)</small>
+        </p>
       </span>
     : <span className="fw-bold text-dark">Extend your rental</span>;
 }
